20b91faf53d0c8ead7252875e2926b92e7c55be9
luv-jeri <luvvjeri@gmail.com>
2022-10-26 22:43:50 +0530

    with new keyword

--- script.js
+++ script.js
@@ -1,14 +1,25 @@
-// # "this keyword" is like a pointer
-// # every function will have its own "this keyword"
-// # In the case of regular function it will point to the object that is calling the function
-// ! KEEP IN MIND only for the regular function i.e. function create using the "function keyword"
-// ! not arrow function ( here story is diffrent )
+// #new keyword
+// * new keyword id going to create a blank object for us
+// * it will change the this keyword poniter to the new object
+// * it is going to make variable points to the new object
+function ProgramingLanguage(name, diff, year, framework, uses) {
+  this.name = name;
+  this.diff = diff;
+  this.year = year;
+  this.framework = framework;
+  this.uses = uses;
 
-const obj = {
-  name: 'John',
-  sayHello: function () {
-    console.log(this);
-  },
-};
+  this.print = function () {
+    console.log('This is ', this.name);
+  };
+  this.hello = "Hello";
+  this.arr = [1,2,4]
 
-obj.sayHello();
+}
+
+const js = new ProgramingLanguage('JavaScript', 'easy', 1995, 'React', 'web');
+const py = new ProgramingLanguage('Python', 'easy', 1991, 'Django', 'web');
+const c = new ProgramingLanguage('C', 'hard', 1972, 'none', 'system');
+
+js.print()
+py.print()
