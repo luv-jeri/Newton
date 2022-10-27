4c3e7552b59b9dd56a0d98c2971d9847c1069769
luv-jeri <luvvjeri@gmail.com>
2022-10-27 20:57:58 +0530

    -

--- script.js
+++ script.js
@@ -1,14 +1,36 @@
-// # "this keyword" is like a pointer
-// # every function will have its own "this keyword"
-// # In the case of regular function it will point to the object that is calling the function
-// ! KEEP IN MIND only for the regular function i.e. function create using the "function keyword"
-// ! not arrow function ( here story is diffrent )
+// const obj = {
+//   name: 'John',
+//   sayHello: function () {
+//     console.log(this);
+//   },
+// };
 
-const obj = {
-  name: 'John',
-  sayHello: function () {
-    console.log(this);
-  },
+// obj.sayHello(); //* {name: "John", sayHello: ƒ}
+// console.log(this);
+// const obj = {
+//   name: 'John',
+//   sayHello: () => {
+//     const say = () => {
+//       console.log(this);
+//     };
+
+//     say();
+//   },
+// };
+
+// obj.sayHello();
+
+const name = 'Rajesh';
+
+const person = (name) => {
+  return {
+    name: name,
+    sayHello: () => {
+      console.log(this);
+      console.log(` -->> ${this.name}`);
+    },
+  };
 };
 
-obj.sayHello();
+const sanjay = person('Sanjay');
+sanjay.sayHello();
