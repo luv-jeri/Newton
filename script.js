ab5449ff435deee61f0524646b5a1535b8974eff
luv-jeri <luvvjeri@gmail.com>
2022-11-04 23:06:45 +0530

    -

--- script.js
+++ script.js
@@ -1,58 +1,61 @@
-// const promise = new Promise((resolve, reject) => {
-//   const button = document.querySelectorAll('button');
-//   if (button.length > 0) {
-//     resolve(button);
-//   } else {
-//     reject("No button in UI");
-//   }
-// });
+const userInput = document.getElementById('user-input');
+const findButton = document.getElementById('find-button');
 
-// promise.then((data) => {
-//   console.log('Promise resolved ', data);
-// });
+class GithubCard {
+  #wrapper = document.getElementById('wrapper');
+  constructor(avatar_url, name, login, bio) {
+    this.avatar_url = avatar_url;
+    this.name = name;
+    this.login = login;
+    this.bio = bio;
+    this.card = null;
+  }
+  create() {
+    const card = document.createElement('div');
+    card.classList.add = 'git-card';
+    const img = document.createElement('img');
+    img.src = this.avatar_url;
+    img.height = 100;
+    img.width = 100;
+    const name = document.createElement('h2');
+    name.innerText = this.name;
+    const a = document.createElement('a');
+    a.href = `http://github.com/${this.login}`;
+    a.innerText = this.login;
+    const login = document.createElement('h3');
+    login.innerText = this.login;
+    const bio = document.createElement('p');
+    bio.innerText = this.bio;
+    const button = document.createElement('button');
+    button.innerText = 'Remove';
+    button.addEventListener('click', () => {
+      this.remove();
+    })
+    card.append(img, name, a, login, bio, button);
+    this.card = card;
+    return card;
+  }
+  render() {
+    this.create();
+    this.#wrapper.append(this.card);
+  }
+  remove() {
+    this.card.remove();
+  }
+}
 
-// promise.catch((err) => {
-//   console.log('We failed 🥲', data);
-// });
+const findUser = () => {
+  const promise = axios.get(`https://api.github.com/users/${userInput.value}`);
 
-const url = 'https://jsonplaceholder.typicode.com/users';
-
-const fetchData = (url) => {
-  const promise = new Promise((resolve, reject) => {
-    const xhr = new XMLHttpRequest();
-    xhr.open('GET', url);
-    xhr.send();
-    xhr.onload = () => {
-      if (xhr.status === 200) {
-        resolve(xhr.response);
-      } else {
-        reject('Something went wrong');
-      }
-    };
-
-    xhr.onerror = () => {
-      reject('Something went wrong');
-    };
+  promise.then((response) => {
+    const userCard = new GithubCard(
+      response.data.avatar_url,
+      response.data.name,
+      response.data.login,
+      response.data.bio
+    );
+    userCard.render();
   });
-  return promise;
 };
 
-const promise = fetchData(url);
-promise.then((data) => {
-  console.log('Promise resolved ', data);
-});
-
-// const promise = fetch(url);
-
-// promise.then((responce) => {
-
-//   const jsonPromise = responce.json();
-//   jsonPromise.then((data) => {
-//     console.log(data);
-//   });
-
-// });
-
-// promise.catch((err) => {
-//   console.log('We failed 🥲', err);
-// });
+findButton.addEventListener('click', findUser);
